ef0bb3cf64642cf3f850e3b1a827579f3d9ad689
Ankit <ankit.sin099@gmail.com>
2021-01-19 12:50:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000);
+
+const secHand = document.querySelector('.clock .second');
+const minHand = document.querySelector('.clock .minute');
+const houHand = document.querySelector('.clock .hour');
+
+function setClock() {
+    const currentTime = new Date();
+    const sec = currentTime.getSeconds() / 60;
+    const min = (sec + currentTime.getMinutes()) / 60;
+    const hou = (min + currentTime.getHours()) / 12;
+
+    setRotation(secHand, sec);
+    setRotation(minHand, min);
+    setRotation(houHand, hou);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
